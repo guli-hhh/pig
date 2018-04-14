e260a9586382a146ef701fc2ea57be11f4ba51f9
BrickCat <549595297@qq.com>
2018-04-14 15:55:12 +0800

    add wx modules

--- pig-modules/pig-wx-service/pig-wx-mp-service/src/main/java/com/github/pig/wx/mp/PigWxMpServiceApplication.java
+++ pig-modules/pig-wx-service/pig-wx-mp-service/src/main/java/com/github/pig/wx/mp/PigWxMpServiceApplication.java
@@ -2,8 +2,14 @@ package com.github.pig.wx.mp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@ComponentScan(basePackages = {"com.github.pig.wx.mp", "com.github.pig.common.bean"})
 public class PigWxMpServiceApplication {
 
     public static void main(String[] args) {
