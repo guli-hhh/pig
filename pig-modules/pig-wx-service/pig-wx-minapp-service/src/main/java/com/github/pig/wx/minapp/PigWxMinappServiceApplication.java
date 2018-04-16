728f49f77f6fc259feefe1e43bbfcf7e13853b36
549595297@qq.com <jia327926>
2018-04-16 17:50:45 +0800

    add wx miniapp

---
+++ pig-modules/pig-wx-service/pig-wx-minapp-service/src/main/java/com/github/pig/wx/minapp/PigWxMinappServiceApplication.java
@@ -0,0 +1,12 @@
+package com.github.pig.wx.minapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigWxMinappServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PigWxMinappServiceApplication.class, args);
+    }
+}
