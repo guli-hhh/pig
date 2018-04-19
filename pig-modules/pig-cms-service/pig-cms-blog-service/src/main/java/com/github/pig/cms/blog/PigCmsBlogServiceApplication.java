cdb42ba18b2e96083fa1f2aabb63c7665820f037
549595297@qq.com <jia327926>
2018-04-19 16:55:26 +0800

    add blog modules

--- pig-modules/pig-cms-service/pig-cms-blog-service/src/main/java/com/github/pig/cms/blog/PigCmsBlogServiceApplication.java
+++ pig-modules/pig-cms-service/pig-cms-blog-service/src/main/java/com/github/pig/cms/blog/PigCmsBlogServiceApplication.java
@@ -2,8 +2,14 @@ package com.github.pig.cms.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@ComponentScan(basePackages = {"com.github.pig.cms.blog", "com.github.pig.common.bean"})
 public class PigCmsBlogServiceApplication {
 
     public static void main(String[] args) {
---
+++ pig-modules/pig-cms-service/pig-cms-common/src/main/java/com/github/pig/cms/common/Article.java
@@ -0,0 +1,11 @@
+package com.github.pig.cms.common;
+
+/**
+ * @author 毛子坤
+ * @Title: ${file_name}
+ * @Package ${package_name}
+ * @Description: ${todo}
+ * @date 2018/4/1916:54
+ */
+public class Article {
+}
