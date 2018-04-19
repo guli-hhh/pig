d21fdfa5f44b420e1365e391398586327a81a79f
549595297@qq.com <jia327926>
2018-04-19 16:42:48 +0800

    cms modules

---
+++ pig-modules/pig-cms-service/pig-cms-blog-service/src/main/java/com/github/pig/cms/blog/PigCmsBlogServiceApplication.java
@@ -0,0 +1,12 @@
+package com.github.pig.cms.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigCmsBlogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PigCmsBlogServiceApplication.class, args);
+    }
+}
---
+++ pig-modules/pig-cms-service/pig-cms-blog-service/src/test/java/com/github/pig/cms/blog/PigCmsBlogServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.pig.cms.blog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PigCmsBlogServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
