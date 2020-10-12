0cd855b599330dd32671499339dfb44b1c3eac9c
tuzhaolian <1129830650@qq.com>
2020-10-12 09:58:33 +0800

    改为docker部署

--- sf-ui/src/config/env.js
+++ sf-ui/src/config/env.js
@@ -5,7 +5,7 @@ const baseUrl = ''
 // 图表库为avue和pig2套地址
 const iconfontVersion = ['567566_qo5lxgtishg', '667895_v7uduh4zui']
 const iconfontUrl = `//at.alicdn.com/t/font_$key.css`
-const isNginx = false
+const isNginx = true
 const codeUrl = isNginx ? `${window.location.origin}/api/code` : `${window.location.origin}/code`
 
 export {
